a443971d8bb9ad442ad367a7a1a73fed9c344fa6
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-06-01 17:56:25 -0600

    import MovieCard and MovieView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
-class MainView extends React.Component {
+export class MainView extends React.Component {
 
   constructor() {
     super();
@@ -9,19 +11,22 @@ class MainView extends React.Component {
         { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...' },
         { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...' },
         { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
-      ]
+      ],
+      selectedMovie: null
     }
   }
 
   render() {
+    const { movies, selectedMovie } = this.state;
+
+    if (selectedMovie) return <MovieView movie={selectedMovie} />;
+
+    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
     return (
       <div className="main-view">
-        <div>Inception</div>
-        <div>The Shawshank Redemption</div>
-        <div>Gladiator</div>
+        {movies.map(movie => <MovieCard key={movie._id} movie={movie} />)}
       </div>
     );
   }
 }
-
-export default MainView;
