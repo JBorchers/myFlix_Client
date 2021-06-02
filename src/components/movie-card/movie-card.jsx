20803da4d26e9ef603dde5756686d5c2b6cb5f53
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-06-01 18:01:27 -0600

    added event listener to MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export class MovieCard extends React.Component {
   render() {
     // 'movie' is the name of the prop used in <MovieCard … />
-    const { movie } = this.props;
-    return <div className="movie-card" >{movie.Title}</div>;
+    const { movie, onMovieClick } = this.props;
+
+    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
   }
 }
