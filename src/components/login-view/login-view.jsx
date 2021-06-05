c816f1a8f5ea7b9b62bc739dccd6e0156264abd6
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-06-05 12:35:51 -0600

    Updated event listener for login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -23,7 +23,7 @@ export function LoginView(props) {
         Password:
         <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
       </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
+      <button type="submit" onClick={e => handleSubmit(e)}>Submit</button>
     </form>
   );
 }
