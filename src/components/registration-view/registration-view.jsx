b43846dc2307c66b8ed79cffe344cc23f7cc78f7
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-06-09 12:47:07 -0600

    updated styling with bootstrap

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -15,23 +15,31 @@ export function RegistrationView(props) {
 
   return (
     <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>
-        Email:
-      <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Birthdate:
-      <input type="text" value={birthdate} onChange={e => setBirthdate(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
+      <div class="form-group">
+        <label>
+          <p>Username:</p>
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </label>
+      </div>
+      <div class="form-group">
+        <label>
+          <p>Password:</p>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+      </div>
+      <div class="form-group">
+        <label>
+          <p>Email:</p>
+          <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+        </label>
+      </div>
+      <div class="form-group">
+        <label>
+          <p>Birthdate:</p>
+          <input type="text" value={birthdate} onChange={e => setBirthdate(e.target.value)} />
+        </label>
+      </div>
+      <button type="submit" class="btn btn-primary mb-2" onClick={handleSubmit}>Submit</button>
     </form>
   );
 }
