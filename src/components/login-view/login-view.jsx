5ecc11ee6b5691bcf3b097f33f8b3e38d2975f96
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-06-13 13:11:12 -0600

    updated handleSubmit login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -9,29 +9,30 @@ export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username), which provides the username to our parent component (child to parent communication) */
-    props.onLoggedIn(username);
-  };
-
   // const handleSubmit = (e) => {
   //   e.preventDefault();
+  //   console.log(username, password);
   //   /* Send a request to the server for authentication */
-  //   axios.post('https://borchers-movie-api.herokuapp.com/login', {
-  //     Username: username,
-  //     Password: password
-  //   })
-  //     .then(response => {
-  //       const data = response.data;
-  //       props.onLoggedIn(data);
-  //     })
-  //     .catch(e => {
-  //       console.log('no such user')
-  //     });
+  //   /* then call props.onLoggedIn(username), which provides the username to our parent component (child to parent communication) */
+  //   props.onLoggedIn(username);
   // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /* Send a request to the server for authentication */
+    axios.post('https://borchers-movie-api.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+      .then(response => {
+        const data = response.data;
+        // triggers onLoggedIn method in main-view.jsx
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('no such user')
+      });
+  };
 
   return (
     <Form>
