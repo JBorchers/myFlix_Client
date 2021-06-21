d33950da129c669f8976ae16551a4f70b370e41a
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-06-21 09:37:44 -0600

    created profile-view

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, FormControl } from 'react-bootstrap';
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import { Button, Form, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export class ProfileView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Username: "",
+      Password: "",
+      Email: "",
+      Birthdate: "",
+      FavoriteMovies: [],
+    };
+  }
+
+  componentDidMount() {
+    let accessToken = localStorage.getItem("token");
+    this.getUsers(accessToken);
+  }
+
+  getMovies(token) {
+    axios.get(`${Config.API_URL}/users`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          users: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+
+  formValidation() {
+    let UsernameError = {};
+    let EmailError = {};
+    let PasswordError = {};
+    let BirthdateError = {};
+    let isValid = true;
+    if (this.state.Username.trim().length < 5) {
+      UsernameError.usernameShort = "Must be alphanumeric and contains at least 5 characters";
+      isValid = false;
+    }
+    if (this.state.Password.trim().length < 3) {
+      PasswordError.passwordMissing = "You must enter a current or new password.(minimum 4 characters) ";
+      isValid = false;
+    }
+    if (!(this.state.Email && this.state.Email.includes(".") && this.state.Email.includes("@"))) {
+      EmailError.emailNotEmail = "A valid email address is required.";
+      isValid = false;
+    }
+    if (this.state.birthdate === '') {
+      BirthdateError.birthdateEmpty = "Please enter your birthdate.";
+      isValid = false;
+    }
+    this.setState({
+      UsernameError: UsernameError,
+      PasswordError: PasswordError,
+      EmailError: EmailError,
+      BirthdateError: BirthdateError,
+    })
+    return isValid;
+  };
+
+
+  render() {
+    const { user, movies } = this.props;
+    const { UsernameError, EmailError, PasswordError, BirthdateError } = this.state;
+    const FavoriteMovieList = movies.filter((movie) => {
+      return this.state.FavoriteMovies.includes(movie._id);
+    });
+    return (
+      <div className="userProfile">
+
+
+      </div>
+
+
+
+
+    )
+  };
+
+};
