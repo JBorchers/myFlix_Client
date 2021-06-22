50fddeac5e08f420c5a8290d133fbd9891215976
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-06-21 18:46:43 -0600

    updated movie-view descriptions

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -26,13 +26,21 @@ export class MovieView extends React.Component {
           <span className="value">{movie.Title}</span>
         </div>
         <div className="movie-description">
+          <span className="label">Release Date: </span>
+          <span className="value">{movie.ReleaseDate}</span>
+        </div>
+        <div className="movie-description">
           <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
         </div>
-        {/* <div className="movie-description">
+        <div className="movie-description">
           <span className="label">Director: </span>
-          <span className="value">{director.Name}</span>
-        </div> */}
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre}</span>
+        </div>
         <button onClick={onBackClick}>Back</button>
 
       </div>
