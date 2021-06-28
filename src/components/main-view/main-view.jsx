54d48608d34417f3d40fc1c81ea4e2e2c707e40c
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-06-28 13:49:51 -0600

    adjusted genre-view routing

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -174,7 +174,7 @@ export class MainView extends React.Component {
             if (!user) return
             if (movies.length === 0) return <div className="main-view" />;
             return <Col md={8}>
-              <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
+              <GenreView genre={movies.find(movie => movie.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
             </Col>
           }} />
 
