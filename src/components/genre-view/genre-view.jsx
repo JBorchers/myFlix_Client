2fb575cd9204877d4d00758f1ead3e8ab98f3e57
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-07-02 09:29:16 -0600

    added "movies in this genre" to genre-view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 import './genre-view.scss';
+import { MovieCard } from "../movie-card/movie-card";
+
 
 export class GenreView extends React.Component {
 
   render() {
-    const { genre, onBackClick, movies } = this.props;
+    const { genre, onBackClick, movies, genresMovies } = this.props;
 
     return (
       <div className="genre-view">
@@ -17,6 +20,19 @@ export class GenreView extends React.Component {
           <span className="label">Description: </span>
           <span className="value">{genre.Description}</span>
         </div>
+        <Col md={6}>
+          <div id="genresMovies">
+
+            <h5>Movies in this genre:</h5>
+            {genresMovies.map(m => {
+              return (
+                <Col md={12} key={m._id}>
+                  <MovieCard movie={m} />
+                </Col>
+              )
+            })}
+          </div>
+        </Col>
         <Button variant="secondary" size="sm" onClick={() => { onBackClick(null); }}>Back</Button>
       </div>
     );
