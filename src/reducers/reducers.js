d04c24c16b82be22e65e6cf5ac0215be1fe49172
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-07-07 12:37:16 -0600

    created reducers

---
+++ src/reducers/reducers.js
@@ -0,0 +1,43 @@
+import { combineReducers } from 'redux';
+
+import { SET_FILTER, SET_MOVIES, ADD_MOVIE, DELETE_MOVIE } from '../actions/actions';
+
+
+// reducer - when concerned by the action, it returns a new value
+function visibilityFilter(state = '', action) {
+  switch (action.type) {
+    case SET_FILTER:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+// reducer
+function movies(state = [], action) {
+  // switches between action types - finds what is concerned
+  switch (action.type) {
+    case SET_MOVIES:
+      return action.value;
+    // case ADD_MOVIE:
+    //   return action.value;
+    // case DELET_MOVIE:
+    //   return action.value;
+    default:
+      return state;
+  }
+}
+
+// function moviesApp(state = {}, action) {
+//   return {
+//     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+//     movies: movies(state.movies, action)
+//   }
+// }
+
+const moviesApp = combineReducers({
+  visibilityFilter,
+  movies
+});
+
+export default moviesApp;
