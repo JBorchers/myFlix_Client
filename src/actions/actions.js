70c84b1b637e747f7e544cc24c96d8f2ffa3f771
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-07-07 12:34:30 -0600

    created actions component

---
+++ src/actions/actions.js
@@ -0,0 +1,21 @@
+// declares action types
+export const SET_MOVIES = 'SET_MOVIES';
+export const ADD_MOVIE = 'ADD_MOVIE';
+export const DELETE_MOVIE = 'DELETE_MOVIE';
+export const SET_FILTER = 'SET_FILTER';
+
+export function setMovies(value) {
+  return { type: SET_MOVIES, value };
+}
+
+export function addMovie(value) {
+  return { type: ADD_MOVIE, value };
+}
+
+export function deleteMovie(value) {
+  return { type: DELETE_MOVIE, value };
+}
+
+export function setFilter(value) {
+  return { type: SET_FILTER, value };
+}
