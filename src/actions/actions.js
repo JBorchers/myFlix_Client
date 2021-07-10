0bee6ce614fe1147d7d7f533642e928085649483
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-07-10 17:14:21 -0600

    added setUser action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,6 +3,7 @@ export const SET_MOVIES = 'SET_MOVIES';
 export const ADD_MOVIE = 'ADD_MOVIE';
 export const DELETE_MOVIE = 'DELETE_MOVIE';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_USER = 'SET_USER';
 
 export function setMovies(value) {
   return { type: SET_MOVIES, value };
@@ -18,4 +19,8 @@ export function deleteMovie(value) {
 
 export function setFilter(value) {
   return { type: SET_FILTER, value };
+}
+
+export function setUser(value) {
+  return { type: SET_USER, value };
 }
